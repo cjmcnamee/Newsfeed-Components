d315164b8b4b095f6d38c6943ca3884e3ebff216
connor mcnamee <connorjamesmcnamee@gmail.com>
2019-07-10 17:14:46 -0400

    finished stretch goals

--- Article/Article.js
+++ Article/Article.js
@@ -2,12 +2,17 @@ class Article {
   constructor(domElement) {
     this.domElement = domElement;
     this.expandButton = domElement.querySelector('.expandButton');
+    this.closeButton = domElement.querySelector('.closeButton');
     this.expandButton.textContent = "expand";
     this.expandButton.addEventListener('click', this.expandArticle.bind(this));
+    this.closeButton.addEventListener('click', this.closeArticle.bind(this));
   }
   expandArticle() {
     console.log('expandArticle()', this)
     this.domElement.classList.toggle('article-open');
+  }
+  closeArticle() {
+    this.domElement.classList.toggle('article-close')
   }
 }
 
